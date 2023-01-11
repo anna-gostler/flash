0a0ba17684160e6f2eda02684b970f78164203b3
anna-gostler <anna-gostler@users.noreply.github.com>
2023-01-11 11:49:24 +0100

    moved proxy config

--- src/app/services/dictionary/dictionary.service.ts
+++ src/app/services/dictionary/dictionary.service.ts
@@ -9,6 +9,7 @@ export class DictionaryService {
   constructor(private http: HttpClient) {}
 
   public getEntry(keyword: string) {
+    console.log('look up keyword', keyword, 'in dictionary');
     const url = `/api/v1/search/words?keyword=${keyword}`;
     console.log('get', url);
     return this.http.get<JishoDictionaryEntry>(url);

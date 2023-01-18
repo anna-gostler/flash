6ab13bad1324a34890f7f74bef5214a2ae338923
anna-gostler <anna-gostler@users.noreply.github.com>
2023-01-18 16:36:29 +0100

    select url based on environment

--- src/app/services/dictionary/dictionary.service.ts
+++ src/app/services/dictionary/dictionary.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, isDevMode } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { JishoDictionaryEntry } from 'src/app/models/dictionary.model';
 
 @Injectable({
@@ -9,12 +9,21 @@ export class DictionaryService {
   constructor(private http: HttpClient) {}
 
   public getEntry(keyword: string) {
-    // TODO choose url based on environment   
-    return this.http.get<JishoDictionaryEntry[]>(
-      `https://flash-app.herokuapp.com/dict?keyword=${keyword}`
+    console.log(
+      'Get dictionary entry. keyword: ',
+      keyword,
+      'isDevMode(): ',
+      isDevMode()
     );
-    //return this.http.get<JishoDictionaryEntry[]>(
-    //  `http://localhost:8080/dict?keyword=${keyword}`
-    //);
+    
+    if (isDevMode()) {
+      return this.http.get<JishoDictionaryEntry[]>(
+        `http://localhost:8080/dict?keyword=${keyword}`
+      );
+    } else {
+      return this.http.get<JishoDictionaryEntry[]>(
+        `https://flash-app.herokuapp.com/dict?keyword=${keyword}`
+      );
+    }
   }
 }
